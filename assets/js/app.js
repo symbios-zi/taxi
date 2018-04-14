6d2409f6955835d7bc9fb2ca9d624daf5c10e26a
symbios-zi <symbios@rootnix.net>
2018-04-15 01:50:02 +0300

    added form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-alert(12323);
+import Vue from 'vue';
+import App from './components/app.vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
+Vue.config.debug = true;
+Vue.config.devtools = true;
+
+new Vue(Vue.util.extend(App)).$mount('app');
---
+++ assets/js/components/app.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="row">
+        <div class="col-4 offset-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Заказ такси</h5>
+                    <div class="form-group">
+                        <autocomplete
+                            url="/api/v1/places/autocomplete"
+                            anchor="description"
+                            label="description"
+                            name="point"
+                            :classes="{ input: 'form-control', wrapper: 'input-wrapper'}"
+                            :process="processJSON"
+                            :onSelect="handleSelectFrom"
+                            :placeholder="'Укажите адрес отправки'"
+                        >
+                        </autocomplete>
+                    </div>
+                    <div class="form-group">
+                        <autocomplete
+                            url="/api/v1/places/autocomplete"
+                            anchor="description"
+                            label="description"
+                            name="destination"
+                            :classes="{ input: 'form-control', wrapper: 'input-wrapper'}"
+                            :process="processJSON"
+                            :onSelect="handleSelectTo"
+                            :placeholder="'Укажите адрес поездки'"
+                        >
+                        </autocomplete>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Autocomplete from 'vue2-autocomplete-js';
+    import axios from 'axios';
+    require('vue2-autocomplete-js/dist/style/vue2-autocomplete.css');
+
+    export default {
+        components: { Autocomplete },
+        data () {
+            return {
+                route: {
+                    from: null,
+                    to: null
+                }
+            }
+        },
+        methods: {
+            processJSON(json) {
+                return json.predictions;
+            },
+            handleSelectFrom({place_id}) {
+                this.route.from = place_id
+            },
+            handleSelectTo({place_id}) {
+                this.route.to = place_id
+            },
+            requestJourneyInformation(data) {
+                axios.post('/journey', {
+                    from: data.from,
+                    to: data.to
+                })
+                    .then(function (response) {
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        },
+        watch: {
+            route: {
+                handler: function (val, oldVal) {
+                    this.requestJourneyInformation(val);
+                },
+                deep: true
+            }
+        }
+    };
+</script>
